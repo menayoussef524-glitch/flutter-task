2a42730ced02ee35a24d8dfd8e06c56b3d03682a
menayoussef524-glitch <menayoussef524@gmail.com>
2026-05-16 12:25:59 +0300

    task 5

---
+++ mina/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+import 'package:task_5/feature/auth/login/screen/login_screen.dart';
+
+void main() {
+  runApp(
+    Sizer(
+      builder: (context, orientation, deviceType) {
+        return const MyApp();
+      },
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
